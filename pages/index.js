5a0e07e6aac6168efba56c2237210082e437076c
saqibilyasrana <rs@adeogroup.co.uk>
2025-03-06 13:29:26 +0500

    meta pixel code added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import Script from 'next/script'
 
 // Components
 import Layout from "../components/layout/Layout";
@@ -19,6 +20,24 @@ const index = () => {
         <title>CxLT Agency</title>
         <meta name="description" content="CxLT" />
         <link rel="icon" href="/favicon.ico" />
+
+        <Script async src="https://www.facebook.com/tr?id=1303973084161841&ev=PageView&noscript=1"></Script>
+        <Script id="meta">
+        {`
+        !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1303973084161841');
+              fbq('track', 'PageView');
+              fbq('track', 'ViewContent');
+        `}
+      </Script>
+
       </Head>
 
       <Layout preloaderDelay={2.5} preloader={true} pageTransition={false} bullets={true}>
