9692d4226482c4836e7636ea8287f4182d0b7c1d
urraish095 <urraish.raza@biomeplatform.co.uk>
2025-04-17 19:28:01 +0500

    Contact form and thankyou page designed

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -42,12 +42,10 @@ const ContactUsPage = () => {
                     </svg>
                 </a>
                 <Link
-                href={{
-                    pathname: "/",
-                }}
+                href={"/"}
                 >
-                <a class="lg:bg-[#1b2934] p-4 rounded-full text-white opacity-100 nav-icon btn-scale false duration-300 ease-in-out"
-                href="/"
+                <button class="lg:bg-[#1b2934] p-4 rounded-full text-white opacity-100 nav-icon btn-scale false duration-300 ease-in-out"
+               
                 >
                     <svg width="50px" height="50px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
 
@@ -58,7 +56,7 @@ const ContactUsPage = () => {
                         <g id="SVGRepo_iconCarrier"> <path d="M9 4.5H8C5.64298 4.5 4.46447 4.5 3.73223 5.23223C3 5.96447 3 7.14298 3 9.5V10M9 19.5H8C5.64298 19.5 4.46447 19.5 3.73223 18.7678C3 18.0355 3 16.857 3 14.5V14" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"/> <path d="M13.6576 2.34736C11.4955 1.97026 10.4145 1.78171 9.70725 2.4087C9 3.03569 9 4.18259 9 6.4764V17.5236C9 19.8174 9 20.9643 9.70725 21.5913C10.4145 22.2183 11.4955 22.0297 13.6576 21.6526L15.9864 21.2465C18.3809 20.8288 19.5781 20.62 20.2891 19.7417C21 18.8635 21 17.5933 21 15.0529V8.94711C21 6.40672 21 5.13652 20.2891 4.25826C19.814 3.67133 19.1217 3.38338 18 3.13228" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"/> <path d="M12 11V13" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"/> </g>
 
                     </svg>
-                </a>
+                </button>
                 
               </Link>
             </div>
@@ -124,7 +122,7 @@ const ContactUsPage = () => {
                                         pathname: "https://www.youtube.com/@CxLTAgency",
                                     }}
                                     >
-                                    <a
+                                    <button
                                       href="https://www.instagram.com/cxlt.agency/"
                                       target="_blank"
                                       rel="noreferrer"
@@ -136,7 +134,7 @@ const ContactUsPage = () => {
                                     width={30}
                                     alt=""
                                   />
-                                    </a>
+                                    </button>
                                     </Link>
                                   </li>
                                   <li>
@@ -145,7 +143,7 @@ const ContactUsPage = () => {
                                         pathname: "https://www.youtube.com/@CxLTAgency",
                                     }}
                                     >
-                                    <a
+                                    <button
                                       href="https://www.facebook.com/cxlt.agency"
                                       target="_blank"
                                       rel="noreferrer"
@@ -157,7 +155,7 @@ const ContactUsPage = () => {
                                     width={30}
                                     alt=""
                                   />
-                                    </a>
+                                    </button>
                                     </Link>
                                   </li>
                                   <li>
@@ -166,7 +164,7 @@ const ContactUsPage = () => {
                                         pathname: "https://www.youtube.com/@CxLTAgency",
                                     }}
                                     >
-                                    <a
+                                    <button
                                       href="https://www.linkedin.com/company/cxlt-agency/"
                                       target="_blank"
                                       rel="noreferrer"
@@ -179,7 +177,7 @@ const ContactUsPage = () => {
                                     alt=""
                                   />
                                       
-                                    </a>
+                                    </button>
                                     </Link>
                                   </li>
                                   <li>
@@ -188,7 +186,7 @@ const ContactUsPage = () => {
                                         pathname: "https://www.youtube.com/@CxLTAgency",
                                     }}
                                     >
-                                    <a
+                                    <button
                                       target="_blank"
                                       rel="noreferrer"
                                       className="href-scale"
@@ -199,7 +197,7 @@ const ContactUsPage = () => {
                                     width={30}
                                     alt=""
                                   />
-                                    </a>
+                                    </button>
                                     </Link>
                                   </li>
                         </ul>
