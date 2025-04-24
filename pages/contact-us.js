e1154d03fbd5fac65c252aeef4a0bfa3042e3941
urraish095 <urraish.raza@biomeplatform.co.uk>
2025-04-24 20:20:45 +0500

    google gtag

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -16,6 +16,47 @@ const ContactUsPage = () => {
             })(window,document,'script','dataLayer','GTM-MJ9GZMX4');
         `}
         </Script>
+        <Script async src="https://www.facebook.com/tr?id=1303973084161841&ev=PageView&noscript=1"></Script>
+      <Script id="meta">
+        {`
+        !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1303973084161841');
+              fbq('track', 'PageView');
+              fbq('track', 'ViewContent');
+        `}
+      </Script>
+      <Script id="linkedin-tag">
+        {`
+          window._linkedin_partner_id = "7009842";
+          window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+          window._linkedin_data_partner_ids.push(window._linkedin_partner_id);
+
+          (function(l) {
+            if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+            window.lintrk.q=[]}
+            var s = document.getElementsByTagName("script")[0];
+            var b = document.createElement("script");
+            b.type = "text/javascript";
+            b.async = true;
+            b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+            s.parentNode.insertBefore(b, s);
+          })(window.lintrk);
+
+          window.lintrk('track', { conversion_id: 20452266 });
+        `}
+      </Script>
+
+      {/* LinkedIn noscript fallback */}
+      <noscript>
+        <img height="1" width="1" style={{ display: 'none' }} alt="" src="https://px.ads.linkedin.com/collect/?pid=7009842&fmt=gif" />
+      </noscript>
         <noscript>
         <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MJ9GZMX4"
           height="0" width="0" style={{ display: 'none', visibility: 'hidden' }} />
